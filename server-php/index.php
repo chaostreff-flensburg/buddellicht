97d5c39fd4ae75dcd172e7acdd623129119649d2
Alexander Eichhorn <alex@fl1rt.net>
2018-04-23 22:27:24 +0200

    fix some php server stuff

--- server-php/index.php
+++ server-php/index.php
@@ -1,40 +1,33 @@
 <?php
-include('artnet.php');
-//$art = new quickArtNet('192.168.0.24',0);
+include('artnet.php'); // http://rmlx.co.uk/quickArtNet.class.txt
 $art = new quickArtNet('192.168.0.255',0);
-/*$state1 = array();
-$state2 = array();
-for($i=0;$i < 36;$i++) // 36 Werte
-{
-	$state1[] = rand(1,254);
-	$state2[] = rand(1,254);
-}*/
 mt_srand (date('U'));
-while(true)
+while(true) // Run4ever // not fancy
 {
 	$state1 = array();
 	$state2 = array();
-	for($i=0;$i < 40;$i++) // 36 Werte
+	for($i=0;$i < 40;$i++) // 39 Werte
 	{
 		$state1[] = mt_rand (1,255);
 		$state2[] = mt_rand (1,255);
 	}
 	$i = 0;
+	// @TODO: do better 
 	foreach($state1 as $k => $t)
 	{
-		if($i % 3)
+		if($i % 3) // red
 			$state1[$k] = 1;
 		$i++;
 	}
 	foreach($state2 as $k => $t)
 	{
-		if($i % 6)
+		if($i % 6) // blue
 			$state2[$k] = 1;
 		$i++;
 	}
 	$art->send($state1);
-	usleep (1000000);
+	usleep (1000000); // 1 sec
 	$art->send($state2);
-	usleep (1000000);
-	//var_dump($state1,$state2);
+	usleep (1000000); // 1 sec
+	//var_dump($state1,$state2); // Debug
 }
